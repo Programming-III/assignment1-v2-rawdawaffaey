d2cf5327296949af15453b85932ba73af436c90b
rawdawaffaey <rawdawaffaee@gmail.com>
2025-11-10 23:46:36 -0800

    Update Person.h

--- Person.h
+++ Person.h
@@ -3,7 +3,19 @@
 
 #include <string>
 using namespace std;
+class Person 
 
+{
+private:
+    string name;
+    int id;
+public:
+   Person(string name,int id); 
+   display();
+   ~Person; 
+        
+};
+#endif
 //#write Person class here
 
 
