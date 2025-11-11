92878180d6fd3ca8b1c5196c48b09f7daa29a13b
rawdawaffaey <rawdawaffaee@gmail.com>
2025-11-10 23:47:36 -0800

    Update Instructor.h

--- Instructor.h
+++ Instructor.h
@@ -3,6 +3,17 @@
 
 #include "Person.h"
 using namespace std;
+class Instructor : Person{
+private:
+    string department;
+    int experienceYears;
+public:
+    Instructor(string department,int experienceYears) : Person(string name,int id); 
+    display();
+    ~Instructor;
+ };
+
+#endif
 //#write Instructor class here
 
 
